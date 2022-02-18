f8d0d76c8b76ed509630e4362046f585262dbba3
nittha <nittha1709@gmail.com>
2022-02-18 14:02:17 +0700

    no message

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import Head from "next/head";
-// import Navbar from "@/components/header"
-// import Footer from "@/components/footer";
-// import Banner from "@/components/banner";
-// import LayoutSpan from "@/components/layout";
-import Quality from "@/components/quality";
-import CoreFeatures from "@/components/core-features";
-import FeatureApp from "@/components/feature-app";
-import Connect from "@/components/connect";
-import HowItWorks from "@/components/how-it-works";
-import Payment from "@/components/payment";
-import Package from "@/components/package";
-import Question from "@/components/question";
+import Navbar from "../components/header"
+import Footer from "../components/footer";
+import Banner from "../components/banner";
+import LayoutSpan from "../components/layout";
+import Quality from "../components/quality";
+import CoreFeatures from "../components/core-features";
+import FeatureApp from "../components/feature-app";
+import Connect from "../components/connect";
+import HowItWorks from "../components/how-it-works";
+import Payment from "../components/payment";
+import Package from "../components/package";
+import Question from "../components/question";
 
 
 export default function Home() {
@@ -21,20 +21,20 @@ export default function Home() {
         <meta name="description" content="basic nextJs figma template by startuplanding" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      {/* <Banner>
+      <Banner>
         <Navbar />
-      </Banner> */}
+      </Banner>
       <Quality />
-      {/* <LayoutSpan>
+      <LayoutSpan>
         <CoreFeatures />
         <FeatureApp />
-      </LayoutSpan> */}
+      </LayoutSpan>
       <HowItWorks />
       <Connect />
       <Payment />
       <Package />
       <Question />
-      {/* <Footer /> */}
+      <Footer />
 
     </div>
   )
