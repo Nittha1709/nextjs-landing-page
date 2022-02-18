ea572ae5f7103371559607f536b3435863cdaac1
nittha <nittha1709@gmail.com>
2022-02-18 15:06:06 +0700

    og header

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import Head from "next/head";
-import Navbar from "../components/header"
-import Footer from "../components/footer";
-import Banner from "../components/banner";
-import LayoutSpan from "../components/layout";
-import Quality from "../components/quality";
-import CoreFeatures from "../components/core-features";
-import FeatureApp from "../components/feature-app";
-import Connect from "../components/connect";
-import HowItWorks from "../components/how-it-works";
-import Payment from "../components/payment";
-import Package from "../components/package";
-import Question from "../components/question";
+import Navbar from "@/components/header"
+import Footer from "@/components/footer";
+import Banner from "@/components/banner";
+import LayoutSpan from "@/components/layout";
+import Quality from "@/components/quality";
+import CoreFeatures from "@/components/core-features";
+import FeatureApp from "@/components/feature-app";
+import Connect from "@/components/connect";
+import HowItWorks from "@/components/how-it-works";
+import Payment from "@/components/payment";
+import Package from "@/components/package";
+import Question from "@/components/question";
 
 
 export default function Home() {
@@ -18,8 +18,14 @@ export default function Home() {
     <div>
       <Head>
         <title>Basic Next App Web landing</title>
-        <meta name="description" content="basic nextJs figma template by startuplanding" />
+        <meta property="og:title" content="Home" />
+        <meta property="og:description" content="basic nextJs figma template by startuplanding" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://nextjs-landing-page-zeta.vercel.app" />
+        <meta property="og:image" content="/og-img.png" />
         <link rel="icon" href="/favicon.svg" />
+
+
       </Head>
       <Banner>
         <Navbar />
